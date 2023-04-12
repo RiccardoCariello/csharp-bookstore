3334da1c8ce08eb7f17eb997315b50c87bc6d2ea
MillionDollarBob <riccardo.c1995@outlook.com>
2023-04-12 15:17:44 +0200

    Primo esercizio

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//-------------------------------------hardcoded info--------------------------------------
+
+string titolo = "Trainspotting";
+string autore = "Irvine Welsh";
+string isbn = "978-8850257973";
+int pagine = 368;
+float peso = 330;
+float larghezza = 13.5f;
+float altezza = 20.5f;
+double profondità = 2.4;  //perchè?
+float valutazioneMedia = 4.5f;
+int numeroRecensioni = 46;
+bool kindle = true;
+bool copertinaFlessibile = true;
+
+//------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+Console.WriteLine("--------------------- LIBRO DI OGGI: " + titolo + " DI " + autore + "------------------------");
+Console.WriteLine("Informazioni generiche: ");
